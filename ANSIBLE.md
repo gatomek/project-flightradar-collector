4d3e0ce6ba8b85c7a2491fbb3ae00913f0008624
Gatomek <tomasz.galinski@gmail.com>
2026-07-26 11:04:23 +0200

    cr

--- ANSIBLE.md
+++ ANSIBLE.md
@@ -1,6 +1,27 @@
 # Setup
+
 ## Castle
+
+### Setup
+
 ```sh
 docker run --rm -v .:/project:ro -e ANSIBLE_CONFIG=/project/ansible/ansible.cfg -w /project/ansible gatomek_ansible ansible-playbook -i inventory.ini playbooks/setup.yml
 ```
 
+### Stop
+
+```sh
+docker run --rm -v .:/project:ro -e ANSIBLE_CONFIG=/project/ansible/ansible.cfg -w /project/ansible gatomek_ansible ansible-playbook -i inventory.ini playbooks/stop.yml
+```
+
+### Start
+
+```sh
+docker run --rm -v .:/project:ro -e ANSIBLE_CONFIG=/project/ansible/ansible.cfg -w /project/ansible gatomek_ansible ansible-playbook -i inventory.ini playbooks/start.yml
+```
+
+### Restart
+
+```sh
+docker run --rm -v .:/project:ro -e ANSIBLE_CONFIG=/project/ansible/ansible.cfg -w /project/ansible gatomek_ansible ansible-playbook -i inventory.ini playbooks/restart.yml
+```
--- src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
+++ src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
@@ -1,7 +1,6 @@
 package pl.gatomek.flightradar.radar.poc.radarcollector.adapter.in.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,7 @@ import pl.gatomek.flightradar.radar.poc.radarcollector.application.port.in.LogPo
 public class AircraftLogRestController {
     private final LogPort logPort;
 
-    @GetMapping(value = "/logs", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/logs")
     public ResponseEntity<AircraftNotification> getAircraftNotification() {
         AircraftNotification aircraftNotification = logPort.getLogs();
         return ResponseEntity.ok( aircraftNotification);
