6ce14e4609dfe3e1a15f38f0f2a9559276cca910
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-08-04 00:41:56 +0200

    No auth processing review (#8)

--- src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/config/SecurityConfig.java
+++ src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -62,7 +63,7 @@ public class SecurityConfig {
                 .cors(cors -> cors
                         .configurationSource(corsConfigurationSource)
                 )
-                .addFilterAfter(new UserLogFilter(), BearerTokenAuthenticationFilter.class)
+                .addFilterAfter(new UserLogFilter(), SecurityContextHolderFilter.class)
                 .addFilterAfter(new RequestIdLogFilter(), UserLogFilter.class)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
